0a2e82c1230094ff032af97830ebea1a6fef8800
sahiba <sahiba1214@gmail.com>
2021-12-13 21:04:22 +0530

    [Sahiba1214] file added Create Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,3 +2,7 @@
 create database payrollservice1
 
 use payrollservice1
+
+/*UC2*/
+create table employee_payroll(id int identity (1,1) primary key, Name varchar(150) NOT NULL, Salary float NOT NULL, Start date NOT NULL);
+exec sp_help employee_payroll;
