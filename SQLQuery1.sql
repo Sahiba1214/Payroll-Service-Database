c9baffc35a58de39f7ffbc3affce8a2e0415b5c5
sahiba <sahiba1214@gmail.com>
2021-12-17 16:49:53 +0530

    [sahiba1214] add Update values

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -48,3 +48,13 @@ select count(Salary) from employee_payroll where Gender='F' group by Gender;
 alter table employee_payroll add Phone varchar(16);
 alter table employee_payroll add Address varchar(100) default 'TBD';
 alter table employee_payroll add Department varchar(50) NOT NULL default ''; 
+
+/*UC9*/
+exec sp_rename 'employee_payroll.Salary', 'BasicPay', 'column';
+alter table employee_payroll add Deductions float, TaxablePay float, IncomeTax float, NetPay float;
+update employee_payroll set Phone='2356409476', Address='Delhi' where Name='Sahiba';
+update employee_payroll set Phone='6733448959', Address='Punjab' where Name='Mantasha';
+update employee_payroll set Phone='9523456760', Address='Kolkata' where Name='Kashish';
+update employee_payroll set Phone='7222227504', Address='Mumbai' where Name='Enayat';
+update employee_payroll set Department='Sales', 'Deductions'=1000, 'TaxablePay'=500, IncomeTax=0.1*(BasicPay), NetPay=BasicPay-1500 where Name='Sahiba' or Name='Mantasha';
+update employee_payroll set Department='Marketing', Deductions=1500, 'TaxablePay'=500, IncomeTax=0.15*(BasicPay), NetPay=BasicPay-2000 where Name='Enayat' or Name='Kashish';
